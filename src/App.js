9cdcc4953648ad1a8f772623691475a5407423f8
michael <michaelhall923@protonmail.com>
2022-12-28 18:04:52 -0500

    Fix react-helmet bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 /** @jsxImportSource @emotion/react */
 import "./App.css";
 import { useState, useEffect, useRef } from "react";
-import { Helmet } from "react-helmet";
+// import { Helmet } from "react-helmet";
+import { Helmet, HelmetProvider } from "react-helmet-async";
 import {
   useNavigate,
   useLocation,
@@ -49,104 +50,106 @@ function ImagePage() {
   }, [copied, fileName]);
 
   return (
-    <div className="py-4 px-4 text-violet-400 font-bold" css={imagePageStyle}>
-      <Helmet>
-        <title>{fileName} | Nostrimg</title>
-      </Helmet>
-      <img
-        className="mx-auto rounded"
-        css={imgStyle}
-        src={`https://i.nostrimg.com/${fileName}`}
-        alt={fileName}
-      />
-      <h3 className="mb-4 py-8">
-        <div>Direct Link:</div>
-        <span
-          className="bg-violet-400 text-indigo-900 p-2 rounded cursor-pointer break-all text-xs md:text-base"
-          onClick={() => setCopied(true)}
-        >
-          https://i.nostrimg.com/{fileName}{" "}
+    <HelmetProvider>
+      <div className="py-4 px-4 text-violet-400 font-bold" css={imagePageStyle}>
+        <Helmet>
+          <title>{fileName} | Nostrimg</title>
+        </Helmet>
+        <img
+          className="mx-auto rounded"
+          css={imgStyle}
+          src={`https://i.nostrimg.com/${fileName}`}
+          alt={fileName}
+        />
+        <h3 className="mb-4 py-8">
+          <div>Direct Link:</div>
           <span
-            css={
-              {
-                // minHeight: "100vh",
-                // display: "flex",
-                // justifyContent: "center",
-                // alignItems: "center",
-              }
-            }
+            className="bg-violet-400 text-indigo-900 p-2 rounded cursor-pointer break-all text-xs md:text-base"
+            onClick={() => setCopied(true)}
           >
-            <button
-              css={{
-                appearance: "none",
-                padding: 8,
-                border: 0,
-                outline: 0,
-                cursor: "pointer",
-              }}
+            https://i.nostrimg.com/{fileName}{" "}
+            <span
+              css={
+                {
+                  // minHeight: "100vh",
+                  // display: "flex",
+                  // justifyContent: "center",
+                  // alignItems: "center",
+                }
+              }
             >
-              <div
+              <button
                 css={{
-                  position: "relative",
-                  height: 16,
-                  width: 16,
+                  appearance: "none",
+                  padding: 8,
+                  border: 0,
+                  outline: 0,
+                  cursor: "pointer",
                 }}
               >
-                <Clippy
+                <div
                   css={{
-                    color: "rgb(192, 192, 192)",
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    strokeDasharray: 50,
-                    strokeDashoffset: copied ? -50 : 0,
-                    transition: "all 300ms ease-in-out",
+                    position: "relative",
+                    height: 16,
+                    width: 16,
                   }}
-                />
-                <Check
-                  // isVisible={copied}
-                  css={{
-                    color: "rgb(64, 192, 0)",
-                    position: "absolute",
-                    top: 0,
-                    left: 0,
-                    strokeDasharray: 50,
-                    strokeDashoffset: copied ? 0 : -50,
-                    transition: "all 300ms ease-in-out",
-                  }}
-                />
-              </div>
-            </button>
+                >
+                  <Clippy
+                    css={{
+                      color: "rgb(192, 192, 192)",
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      strokeDasharray: 50,
+                      strokeDashoffset: copied ? -50 : 0,
+                      transition: "all 300ms ease-in-out",
+                    }}
+                  />
+                  <Check
+                    // isVisible={copied}
+                    css={{
+                      color: "rgb(64, 192, 0)",
+                      position: "absolute",
+                      top: 0,
+                      left: 0,
+                      strokeDasharray: 50,
+                      strokeDashoffset: copied ? 0 : -50,
+                      transition: "all 300ms ease-in-out",
+                    }}
+                  />
+                </div>
+              </button>
+            </span>
           </span>
-        </span>
-      </h3>
-      {location.state?.lightningPaymentLink ? (
-        <a href={location.state.lightningPaymentLink}>
-          <div className="flex flex-col md:flex-row items-center justify-center">
-            <QRCodeSVG
-              className="p-1 rounded bg-violet-400 mb-2 md:mb-0"
-              value={location.state.lightningPaymentLink}
-              imageSettings={{
-                src: "https://i.nostrimg.com/e59c502d.png",
-                width: 20,
-                height: 20,
-                excavate: true,
-              }}
-            />
-            <div className="flex flex-col">
-              <h3 className="text-yellow-500 md:ml-16 mb-2 text-xs md:text-base">
-                {/* Lightning: {location.state.lightningDestination} */}
-                <RiArrowLeftCircleLine className="hidden md:inline" /> Tip Us
-                Via Lightning
-              </h3>
-              <div className="bg-violet-400 text-indigo-900 p-2 rounded cursor-pointer break-all text-xs md:text-base md:ml-16">
-                Open In Wallet
+        </h3>
+        {location.state?.lightningPaymentLink ? (
+          <a href={location.state.lightningPaymentLink}>
+            <div className="flex flex-col md:flex-row items-center justify-center">
+              <QRCodeSVG
+                className="p-1 rounded bg-violet-400 mb-2 md:mb-0"
+                value={location.state.lightningPaymentLink}
+                imageSettings={{
+                  src: "https://i.nostrimg.com/e59c502d.png",
+                  width: 20,
+                  height: 20,
+                  excavate: true,
+                }}
+              />
+              <div className="flex flex-col">
+                <h3 className="text-yellow-500 md:ml-16 mb-2 text-xs md:text-base">
+                  {/* Lightning: {location.state.lightningDestination} */}
+                  <RiArrowLeftCircleLine className="hidden md:inline" /> Tip Us
+                  Via Lightning
+                </h3>
+                <div className="bg-violet-400 text-indigo-900 p-2 rounded cursor-pointer break-all text-xs md:text-base md:ml-16">
+                  Open In Wallet
+                </div>
               </div>
             </div>
-          </div>
-        </a>
-      ) : null}
-    </div>
+          </a>
+        ) : null}
+      </div>
+    </HelmetProvider>
   );
 }
 
@@ -480,22 +483,24 @@ function AuthModal({ setIsAuthenticatedCallback }) {
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App bg-indigo-900">
-        <Helmet>
-          <title>Nostrimg</title>
-          <meta
-            name="description"
-            content="Nostrimg is Nostr-native image hosting service."
-          />
-          <meta name="keywords" content="image hosting, nostr" />
-        </Helmet>
-        <Routes>
-          <Route path="/" element={<FileUploader />} />
-          <Route path="/i/:fileName" element={<ImagePage />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <HelmetProvider>
+      <BrowserRouter>
+        <div className="App bg-indigo-900">
+          <Helmet>
+            <title>Nostrimg</title>
+            <meta
+              name="description"
+              content="Nostrimg is Nostr-native image hosting service."
+            />
+            <meta name="keywords" content="image hosting, nostr" />
+          </Helmet>
+          <Routes>
+            <Route path="/" element={<FileUploader />} />
+            <Route path="/i/:fileName" element={<ImagePage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </HelmetProvider>
   );
 }
 
